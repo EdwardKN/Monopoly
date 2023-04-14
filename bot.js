056152fce6b57253501e28f7922369d7c69e719d
ItzAEzClap <karlsson_2@outlook.com>
2023-04-14 23:27:41 +0200

    sync

--- bot.js
+++ bot.js
@@ -5,13 +5,14 @@ class Bot{
 
     async update() {
         if (this.player !== players[turn]) { return }
+        if (this.player.negative) { this.handleBankrupt(); return }
         if (this.player.inJail) { this.handleJail(); return }
         if(this.player.animationOffset == 0  && board.showDices === false && board.animateDices === false){
             await this.player.rollDice()
-            this.hasGroup()
+            if (this.player.negative) { this.handleBankrupt(); return }
+
             let boardPiece = board.boardPieces[this.player.steps]
-            console.log(`${turn + 1}: ` + `${this.getAvergePrice(this.player.steps)}`)
-            if (boardPiece.owner === undefined && this.player.money >= boardPiece.piece.price) {
+            if (boardPiece.owner === undefined && this.player.money - boardPiece.piece.price > 2 * this.getAvergePrice()) {
                 this.buyPiece(boardPiece)
             }
         }
@@ -22,9 +23,29 @@ class Bot{
             this.player.money -= 50
             this.player.getOutOfJail()
         } else {
-            
+            let dice1 = randomIntFromRange(1, 6)
+            let dice2 = randomIntFromRange(1, 6)
+            console.log(dice1, dice2)
+            if (dice1 === dice2) {
+                this.player.getOutOfJail()
+            }
         }
+    }
 
+    // Morgtage
+    // Sell House
+    // Morgtage
+    // Sell Everything
+    handleBankrupt() {
+        while (this.player.money < 0) {
+            for (const bP of this.player.ownedPlaces) {
+                if (!this.hasGroup(bP.piece.group)) {
+                    bP.mortgaged = true
+                    this.player.money += bP.piece.price / 2
+                }
+            }
+        }
+        this.player.negative = false
     }
 
     buyPiece(boardPiece) {
@@ -39,20 +60,54 @@ class Bot{
         let utilitysOwned = this.player.ownedPlaces.filter(bP => bP.piece.type === 'utility').length
         for (let i = 1; i <= 12; i++) {
             let boardPiece = board.boardPieces[(step + i) % 40]
-            if (!boardPiece.owner) { continue }
-            // Station And Utility
+            if (!boardPiece || !boardPiece) { continue }
             if (boardPiece.piece.type === 'station') {
                 totalPrice += 25 * Math.pow(boardPiece.piece.price, stationsOwned)
             } else if (boardPiece.piece.type === 'utility') {
                 totalPrice += this.player.dS * (utilitysOwned === 1 ? 4 : 10)
-            } else if (boardPiece.piece.hasOwnProperty('group')) {
+            } else if (boardPiece.owner !== this.player) {
                 totalPrice += boardPiece.piece.price
             }
         }
         return totalPrice / 12
     }
 
-    hasGroup(group) {
-
+    whoHasAGroup() {
+        let hasbulla = {}
+        players.forEach((player, i) => {
+            let ownsAll = []
+            let cur
+            let all = false
+            for (const bP of board.boardPieces) {
+                if (!bP || !bP.piece.group) { continue }
+                if (bP.piece.group !== cur) {
+                    if (all) { ownsAll.push(cur) }
+                    all = true
+                    cur = bP.piece.group
+                }
+                if (bP.owner !== player) { all = false }
+            }
+            if (ownsAll.length) { hasbulla[i] = [ownsAll] }
+        })
+        return hasbulla
     }
+
+    hasGroup(group) {
+        return (this.whoHasAGroup()[turn] || []).includes(group)
+    }
+
+    getPieceWithHouse() {
+        let hasHouse = []
+        for (const bP of this.player.ownedPlaces) {
+            if (bP.level) { hasHouse.push(bP) }
+        }
+        return hasHouse
+    }
+}
+
+function cha() {
+    players[0].teleportTo(1)
+    board.boardPieces[1].owner = players[0]
+    players[0].teleportTo(3)
+    board.boardPieces[3].owner = players[0]
 }
--- game.js
+++ game.js
@@ -169,6 +169,7 @@ function randomIntFromRange(min, max) {
 };
 
 function playSound(sound,volume){
+    return
     if(sound.type === "single"){
         let myClonedAudio = sound.sound.cloneNode();
         myClonedAudio.volume = volume;
@@ -197,8 +198,8 @@ function init(){
     let botAmount = -2;
 
     if(fastLoad === true){
-        playerAmount = 2;
-        botAmount = -1
+        playerAmount = -1;
+        botAmount = 2
     }
 
     let playerImages = [0,1,2,3,4,5,6,7]
