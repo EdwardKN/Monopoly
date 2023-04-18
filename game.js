d2fb7a26a8b9e3c737a852dcce6cfb6f1c71913e
Edward <edward.klinghammer@gmail.com>
2023-04-18 19:18:48 +0200

    fix

--- game.js
+++ game.js
@@ -853,7 +853,7 @@ class PlayerBorder{
                     }
                     drawRotatedImage(this.x*drawScale,this.y*drawScale + 53*drawScale*1.5 +27*drawScale*this.player.ownedPlaces.length,260*drawScale ,27*drawScale,images.playerOverlay.img[10],0,this.button.mirror,0,0,260,27,false)
                     drawRotatedImage(this.x*drawScale,this.y*drawScale + 53*drawScale*1.5 +27*drawScale*(this.player.ownedPlaces.length+1),260*drawScale ,27*drawScale,images.playerOverlay.img[9],0,this.button.mirror,0,0,260,27,false)
-                    if(players[turn] !== this.player && board.trade === undefined){
+                    if(players[turn] !== this.player && board.trade === undefined && players[turn].bot === undefined){
                         this.createTradebutton.visible = true;
                     }else{
                         this.createTradebutton.visible = false;
